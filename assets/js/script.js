842633aa17310accec8df36a9bbf1016ce3a118d
Joshua Delgado <joshuadelgado073@gmail.com>
2024-02-15 21:49:44 +0800

    Second Commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,15 @@
+AOS.init();
+
 // NavBar Background Scroll //
 window.onscroll = function() {scrollFunction()};
 		 
 	function scrollFunction() {
-		if (document.documentElement.scrollTop > 250) {
+		if (document.documentElement.scrollTop > 100) {
 				
-		document.getElementById("header").style.background = "#000000c5";
+		document.getElementById("header").style.background = "#ececec";
 		} else {
 			   
 		document.getElementById("header").style.background = "none";
 		}
-	}
-
-
-// Typing Animation //
-var typed = new Typed('.typing', {
-  strings: ["Front-End Web Developer"],
-  typeSpeed: 80,
-  backSpeed: 50,
-  loop: true
-});
+}
 
